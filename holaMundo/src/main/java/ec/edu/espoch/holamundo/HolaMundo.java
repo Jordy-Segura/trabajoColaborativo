fa1e5e0d37a92e6ab213ab4f0adf20f94f62d315
Pedro Aguilar <pedro_stalyn@hotmail.com>
2024-11-20 10:03:23 -0500

    Agrege un msm de bienvenida

---
+++ holaMundo/src/main/java/ec/edu/espoch/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.holamundo;
+
+/**
+ *
+ * @author Pedro
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Bienvenidos");
+    }
+}
